bdb9afb59f5dc8cde431e9c2a60cb993599abbf0
Alexis Josephides <ajosephides@alexiss-mbp.home>
2019-01-26 13:13:01 +0000

    reorganised file structure

---
+++ bin/tfl_run.rb
@@ -0,0 +1,25 @@
+class TflRun
+
+  require 'database/database'
+  require 'tflParse'
+  require 'api/tfl_api'
+
+  tubeDb = Database.new
+  tubeDb.createDb
+  tubeDb.createTubeLineTable
+  tubeDb.createTubeStopTable
+  tubeDb.createTubeStopTubeLineTable
+
+  tubeLineApi = TflApi.new
+  tubeLineParse = TflJsonParse.new
+  parseReturn = tubeLineParse.createTubeLine(tubeLineApi.tubeLines)
+  parseReturn.each{|i| tubeDb.addTubeLine(i)}
+
+  tubeStopApi = TflApi.new
+  tubeStopParse = TflJsonParse.new
+  tubeStops = parseReturn.map{|i| tubeStopParse.createTubeStop(tubeStopApi.tubeStops(i))}
+  tubeStops.each{|i| i.each{|j| tubeDb.addTubeStop(j)}}
+  tubeStops.each{|i| i.each{|j| tubeDb.addTubeStopTubeLine(j)}}
+
+
+end
