0be37c6a3bcbbcdc73553186cde130c9fd8d0e9e
Alexis Josephides <ajosephides@gmail.com>
2019-05-24 16:09:34 +0100

    setup class Timetable

---
+++ lib/tube/timetable.rb
@@ -0,0 +1,15 @@
+require 'api/tfl_api'
+
+
+class Timetable
+  attr_accessor :fromStationId, :toStationId, :lineId, :direction, :interval
+
+  def initialize(fromStationId, toStationId, lineId, direction, interval)
+    @fromStationId = String(fromStationId) 
+    @toStationId = String(toStationId)
+    @lineId = String(lineId)
+    @direction = String(direction)
+    @interval = Integer(interval)
+  end
+
+end
